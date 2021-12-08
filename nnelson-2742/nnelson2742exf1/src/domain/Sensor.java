9635371ed2e4483f3ea8b77d14a29843dec7f1da
Nicole PC <nicole.nelson@my.southeastmn.edu>
2021-12-08 05:16:14 -0600

    completed Ex2f

--- nnelson-2742/nnelson2742exf1/src/domain/Sensor.java
+++ nnelson-2742/nnelson2742exf1/src/domain/Sensor.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class Sensor {
-    private int sensorId;
-    private int roomId;
-    private int sensorTypeId;
-    private String description;
-    private Room room;
-    private SensorType sensorType;
-    private ArrayList<SensorReading> sensorReadings;
+    public int sensorId;
+    public int roomId;
+    public int sensorTypeId;
+    public String description;
+    public Room room;
+    public SensorType sensorType;
+    public ArrayList<SensorReading> sensorReadings;
 
     public Sensor(int sensorId, int roomId, int sensorTypeId, String description) {
         this.sensorId = sensorId;
@@ -89,5 +89,102 @@ public class Sensor {
                 ", roomId=" + roomId +
                 ", sensorTypeId=" + sensorTypeId +
                 ", " + description;
+    }
+
+    public ArrayList<SensorReading> getSensorReadings() {
+        return sensorReadings;
+    }
+
+    public void setSensorReadings(ArrayList<SensorReading> sensorReadings) {
+
+        this.sensorReadings = sensorReadings;
+    }
+
+    public int findMinReadingIndex() {
+        float min = sensorReadings.get(0).getValue();
+        int index = 0;
+        for (int i = 1; i < sensorReadings.size(); i++) {
+            if (sensorReadings.get(i).getValue() < min) {
+                min = sensorReadings.get(i).getValue();
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public int findMinReadingIndex(int startIndex, int endIndex) {
+        if (startIndex < 0 || endIndex < 0 || endIndex >= this.sensorReadings.size() || startIndex > endIndex) {
+            throw new IndexOutOfBoundsException("Index out of bounds: 0 - " + (this.sensorReadings.size() - 1)); }
+        else {
+            float min = sensorReadings.get(startIndex).getValue();
+            int index = startIndex;
+            for (int i = startIndex + 1; i <= endIndex; i++) {
+                if (sensorReadings.get(i).getValue() < min) {
+                    min = sensorReadings.get(i).getValue();
+                    index = i;
+                }
+            }
+            return index;
+        }
+    }
+
+    public int findMaxReadingIndex() {
+        float max = sensorReadings.get(0).getValue();
+        int index = 0;
+        for (int i = 1; i < sensorReadings.size(); i++) {
+            if (sensorReadings.get(i).getValue() > max) {
+                max = sensorReadings.get(i).getValue();
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public int findMaxReadingIndex(int startIndex, int endIndex) {
+        if (startIndex < 0 || endIndex < 0 || endIndex >= this.sensorReadings.size() || startIndex > endIndex) {
+            throw new IndexOutOfBoundsException("Index out of bounds: 0 - " + (this.sensorReadings.size() - 1)); }
+        else {
+            float max = sensorReadings.get(startIndex).getValue();
+            int Index = startIndex;
+            for (int i = startIndex + 1; i <= endIndex; i++) {
+                if (sensorReadings.get(i).getValue() > max) {
+                    max = sensorReadings.get(i).getValue();
+                    Index = i;
+                }
+            }
+            return Index;
+        }
+
+    }
+
+    public int findNextCycleMaxIndex(int startIndex){
+        SensorReading rMax = this.sensorReadings.get(startIndex);
+        int index = startIndex + 1;
+        for ( ; index < this.sensorReadings.size(); index++){
+            if(rMax.getValue() < this.sensorReadings.get(index).getValue()){
+                rMax = this.sensorReadings.get(index);
+            }
+            else{
+                break;
+            }
+        }
+        return index -1;
+    }
+
+    public int findNextCycleMinIndex(int startIndex){
+        SensorReading rMin = this.sensorReadings.get(startIndex);
+        int index = startIndex + 1;
+        for ( ; index < this.sensorReadings.size(); index++){
+            if(rMin.getValue() > this.sensorReadings.get(index).getValue()){
+                rMin = this.sensorReadings.get(index);
+            }
+            else{
+                break;
+            }
+        }
+        return index -1;
+    }
+    public SensorReading getSensorReading (int index){
+        return this.sensorReadings.get(index);
     }
 }
--- nnelson-2742/nnelson2742exf1/src/ui/Main.java
+++ nnelson-2742/nnelson2742exf1/src/ui/Main.java
@@ -1,6 +1,7 @@
 package ui;
 
 import dataaccess.SensorReadingJSONParser;
+import domain.Sensor;
 import domain.SensorReading;
 
 import java.util.ArrayList;
@@ -8,15 +9,80 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<SensorReading> sensorReadings = new ArrayList<SensorReading>();
+        Sensor sensor = new Sensor(2,1,1,"Heat Register");
 
         try {
             SensorReadingJSONParser.readFile("D:\\2742 Java II\\nnelson-2742\\nnelson2742exf1/resources/readings.json");
-            sensorReadings = SensorReadingJSONParser.getSensorReadings();
+            sensor.setSensorReadings(SensorReadingJSONParser.getSensorReadings());
         } catch (Exception e) {
             System.out.println(e);
         }
-        for (SensorReading r : sensorReadings) {
-            System.out.println(r.toString());
+//        for (SensorReading r : sensorReadings) {
+//            System.out.println(r.toString());
+//       }
+
+        int index = sensor.findMinReadingIndex();
+        System.out.println("\nMin: index = " + index + ", "+ sensor.getSensorReadings().get(index).toString());
+        index = sensor.findMaxReadingIndex();
+        System.out.println("\nMax: index = " + index + ", "+ sensor.getSensorReadings().get(index).toString());
+
+        index = sensor.findMinReadingIndex(84,95);
+        System.out.println("\nMin between index 84 and 95: index = " + index + ", "+ sensor.getSensorReadings().get(index).toString());
+        index = sensor.findMaxReadingIndex(8,18);
+        System.out.println("\nMax between index 8 and 18: index = " + index + ", "+ sensor.getSensorReadings().get(index).toString());
+
+//        //invalid indexes
+//        index = sensor.findMaxReadingIndex(-1,95);
+//        System.out.println("\nMin between index 84 and 95: index = " + index + ", "
+//                + sensor.getSensorReadings().get(index).toString());
+//        index = sensor.findMaxReadingIndex(8,1000);
+//        System.out.println("\nMax between index 8 and 18: index = " + index +
+//                ", "+ sensor.getSensorReadings().get(index).toString());
+
+        try{
+            index = sensor.findMaxReadingIndex(-1,10);
         }
+        catch (IndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+
+        try{
+            index = sensor.findMaxReadingIndex(1,-1);
+        }
+        catch (IndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+
+        try{
+            index = sensor.findMinReadingIndex(10,9);
+        }
+        catch (IndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+
+        try{
+            index = sensor.findMinReadingIndex(100,118);
+        }
+        catch (IndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+
+        index = sensor.findNextCycleMaxIndex(0);
+        System.out.println("First max after index 0: index = " + index + ", "+ sensor.getSensorReadings().get(index).toString());
+
+        index = sensor.findNextCycleMinIndex(5);
+        System.out.println("First max after index 5: index = " + index + ", "+ sensor.getSensorReadings().get(index).toString());
+
+        int prevIndex = 0;
+        index=sensor.findNextCycleMaxIndex(0);
+        while (index > prevIndex){
+            prevIndex = index;
+            System.out.println("Index: " + index + ", Max: " + sensor.getSensorReading(index));
+            index = sensor.findNextCycleMinIndex(index);
+            if(index > prevIndex)
+                System.out.println("Index: " + index + ", Min: " + sensor.getSensorReading(index));
+            index = sensor.findNextCycleMaxIndex(index);
+        }
+
     }
 }
